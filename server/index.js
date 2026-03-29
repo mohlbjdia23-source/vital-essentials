942eb0329c0b7bb8db96b84e16faa69c64541f3a
mohlbjdia23-source <mohlbjdia23@gmail.com>
2026-03-28 21:55:36 -0700

    Setup Express/MongoDB server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
